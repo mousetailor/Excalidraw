1cd7662b87ef9698a7a6163a3e4710bd5add8786
mousetailor <anantkushwaha23456@gmail.com>
2025-12-30 12:10:51 +0530

    Initial setup

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
+
+const app = express();
+
+app.post("/signup", (req,res) => {
+
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message : "Incorrect credentials",
+    });
+    return;
+  }
+  res.json({
+    userId : "123"
+  })
+
+})
+
+app.post("/signin", (req,res) => {
+
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message : "Incorrect credentials",
+    });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign({userId},JWT_SECRET);
+
+  res.json({token});
+})
+
+app.post("/room",middleware, (req,res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message : "Incorrect credentials",
+    });
+    return;
+  }
+  res.json({
+      roomId : "123"
+  });
+})
+
+app.listen(3001);
